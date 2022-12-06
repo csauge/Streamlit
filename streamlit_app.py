2b81c365f27d3556e238bd20a69e2a629db8a9b6
Christophe Saugé <csauge@gmail.com>
2022-12-06 17:45:51 +0100

    Test with map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import pandas as pd
+import requests
 
-st.write("# Hello\nWorld !")
+st.write("# Liste des Vélib'")
+
+url = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=1500'
+data = requests.get(url).json()
+df = pd.json_normalize(data, "records")
+df[['lat','lon']] = pd.DataFrame(df["fields.coordonnees_geo"].tolist(), index=df.index)
+df
+st.map(df)
+data
